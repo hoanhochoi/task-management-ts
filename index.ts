987b528c74aaf3bf6fef48eafcc8daf104420c1e
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-09-25 20:03:25 +0700

    tao route

--- index.ts
+++ index.ts
@@ -1,31 +1,15 @@
-import express,{Express,Request,Response} from "express";
+import express,{Express} from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Task from "./models/task.model";
-
+import mainV1Routes from "./api/v1/routes/index.route";
 dotenv.config();
 database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
-app.get("/tasks", async (req : Request,res: Response)=>{
-    const tasks = await Task.find({
-        deleted: false,
-    })
-    console.log(tasks)
-    res.json(tasks)
-})
+mainV1Routes(app);
 
-
-app.get("/tasks/detail/:id", async (req: Request,res: Response)=>{
-    const task = await Task.findOne({
-        _id: req.params.id,
-        deleted: false
-    })
-    res.json(task)
-
-})
 
 app.listen(port,()=>{
     console.log(`app listening o port ${port}`)
