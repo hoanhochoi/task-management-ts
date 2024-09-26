72a165eda974af717998dfb30be659e7122b76de
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-09-26 20:25:55 +0700

    bộ lọc theo trạng thái

---
+++ api/v1/controllers/task.controller.ts
@@ -0,0 +1,27 @@
+import { Express,Request,Response } from "express";
+import Task from "../../../models/task.model"
+
+export const  index = async (req : Request,res: Response)=>{
+    interface Find{
+        deleted: boolean,
+        status?: string,
+    }
+    let find: Find = {
+        deleted: false,
+    }
+    if(req.query.status){
+        find.status = req.query.status.toString();
+    }
+    const tasks = await Task.find(find)
+    res.json(tasks)
+}
+
+export const detail = async (req: Request,res: Response)=>{
+    const id: string = req.params.id;
+    const task = await Task.findOne({
+        _id: id,
+        deleted: false
+    })
+    res.json(task)
+
+}
